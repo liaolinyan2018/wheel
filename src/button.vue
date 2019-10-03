fb2171c65dca4426916f27a1233cf00a7b08a721
liaolinyan <linyan.lao@gmail.com>
2019-10-03 10:35:41 +0800

    添加button基本样式

--- src/button.vue
+++ src/button.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="g-button">
-        <button>按钮</button>
+    <div>
+        <button class="g-button">按钮</button>
     </div>
 </template>
 
@@ -10,6 +10,25 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    .g-button {
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
 
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+
+        &:active {
+            background: var(--button-active-bg);
+        }
+
+        &:focus {
+            outline: none;
+        }
+    }
 </style>
