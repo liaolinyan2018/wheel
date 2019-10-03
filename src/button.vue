eb9318aa5b4abc11830ece729d79e23b1a3f8984
liaolinyan <linyan.lao@gmail.com>
2019-10-03 15:45:00 +0800

    添加icon，添加icon属性和iconPosition属性，添加loading

--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,45 @@
 <template>
     <div>
-        <button class="g-button">按钮</button>
+        <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+            <g-icon name="loading" class="loading icon" v-if="icon"></g-icon>
+            <g-icon :name="icon" class="icon" v-if="icon"></g-icon>
+            <div class="text">
+                <slot></slot>
+            </div>
+        </button>
     </div>
 </template>
 
 <script>
+    import GIcon from "./icon";
+
     export default {
-        name: "g-button"
+        name: "g-button",
+        components: {GIcon},
+        // props: ['icon', 'iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator(value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 
 <style lang="scss">
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    };
+
     .g-button {
         height: var(--button-height);
         padding: 0 1em;
@@ -18,6 +47,9 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
 
         &:hover {
             border-color: var(--border-color-hover);
@@ -29,6 +61,38 @@
 
         &:focus {
             outline: none;
+        }
+
+        > .icon {
+            width: 1em;
+            height: 1em;
+            fill: currentColor;
+            overflow: hidden;
+            order: 1;
+
+        }
+
+        > .text {
+            order: 2;
+            line-height: 1em;
+            margin-left: .1em;
+            margin-right: 0;
+        }
+
+        &.icon-right {
+            > .text {
+                order: 1;
+                margin-right: .1em;
+                margin-left: 0;
+            }
+
+            > .icon {
+                order: 2;
+            }
+        }
+
+        .loading {
+            animation: spin 2s infinite linear;
         }
     }
 </style>
---
+++ src/icon.vue
@@ -0,0 +1,16 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href="`#i-${name}`"></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        name: "g-icon",
+        props: ['name']
+    }
+</script>
+
+<style scoped>
+
+</style>
